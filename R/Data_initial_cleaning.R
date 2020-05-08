2a456e96ada9a325404f3713aca045340e58c345
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2020-05-08 15:14:07 -0400

    Cleaning of the PC river done

--- R/Data_initial_cleaning.R
+++ R/Data_initial_cleaning.R
@@ -35,23 +35,66 @@ SMR.raw[, `:=`(Site      = as.integer(SiteNew),
                GPS       = as.integer(GPS),
                areTD     = as.logical(areTD),
                areMB     = as.logical(areMB),
+               BRPercent = as.numeric(BRPercent),
                nFry      = as.integer(nFry),
                nParr     = as.integer(nParr),
                nSalmon   = as.integer(nSalmon),
                nRHCA     = as.integer(nRHCA),
                nSAFO     = as.integer(nSAFO),
                nUnknown  = as.integer(nUnknown),
-               SiteNew   = NULL,                   # Remove SiteNew
-               BRPercent = NULL                    # Remove BRPercent
+               SiteNew   = NULL                   # Remove SiteNew
 )]
 
 # Reordering the data.table
 SMR.raw <- SMR.raw[order(Site, Parcelle), ]
 
 # Replaces NAs by 0 in number of fishes
-SMR.raw[is.na(nFry), nFry := 0]
-SMR.raw[is.na(nParr), nParr := 0]
+SMR.raw[is.na(nFry),    nFry := 0]
+SMR.raw[is.na(nParr),   nParr := 0]
 SMR.raw[is.na(nSalmon), nSalmon := 0]
 
 # Save the file in the data folder
 saveRDS(SMR.raw, 'data/SMR_2017_field_data_clean.Rds')
+
+##### Part 2 : Importation of field data for the Petite Cascapedia River
+
+# Importation of PCR data
+PCR.raw <- read_xlsx(path      =  "data/field data/Field_data_summer_2017_PCR.xlsx",
+                     sheet     =  1,
+                     skip      =  2,
+                     col_names =  T)
+
+# Convert to a data.table
+setDT(PCR.raw)
+
+# Check the class of the variables
+sapply(PCR.raw, unique)
+
+# Change some class of the variables and delete some
+PCR.raw[, `:=`(Site         = as.integer(SiteNew),
+               Transect     = as.integer(Transect),
+               Parcelle     = as.integer(Parcelle),
+               GPS          = as.integer(GPS),
+               areTD        = as.logical(areTD),
+               areMB        = as.logical(areMB),
+               nFry         = as.integer(nFry),
+               nParr        = as.integer(nParr),
+               nSalmon      = as.integer(nSalmon),
+               nPCDPCNMini  = as.integer(nPCDPCNMini),
+               nPCD         = as.integer(nPCD),
+               nPCN         = as.integer(nPCN),
+               nSAFO        = as.integer(nSAFO),
+               nUnknown     = as.integer(nUnknown),
+               SiteNew      = NULL                   # Remove SiteNew
+)]
+
+# Reordering the data.table
+PCR.raw <- PCR.raw[order(Site, Parcelle), ]
+
+# Replaces NAs by 0 in number of fishes
+PCR.raw[is.na(nFry), nFry := 0]
+PCR.raw[is.na(nParr), nParr := 0]
+PCR.raw[is.na(nSalmon), nSalmon := 0]
+
+# Save the file in the data folder
+saveRDS(PCR.raw, 'data/PCR_2017_field_data_clean.Rds')
