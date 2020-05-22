387c0ff020a15f3c4bfbb06832c532b379f52b70
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2020-05-22 14:01:16 -0400

    Initial commit on Data_fun_obs.R

---
+++ R/Data_fun_obs.R
@@ -0,0 +1,12 @@
+################################################################################
+#    Predicting habitat suitability curves through FDA 🏞                      #
+#    Created by :     Jeremie Boudreault (Jeremie.Boudreault@ete.inrs.ca)      #
+#    Current file :   Data_fun_obs.R                                           #
+#    Aim :            Generate functional observations of HSC curves           #
+################################################################################
+
+# Part 0 : Libraries, functions and variable definition ------------------------
+
+# Import library
+library("data.table")    # Working with data.table
+library("ggplot2")        # Plotting in ggplot
