09bc26730c9afd985dc7e6d737d5b7ba232ec2e3
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2020-05-23 12:08:34 -0400

    Generated final output

--- R/Data_selection.R
+++ R/Data_selection.R
@@ -41,3 +41,25 @@ RIVERDATA_SITE[, list(nSites_Fry = sum(nObs_Fry >= eval(nObs.min)),
 nObs.min <- 4
 Fish.var <- 'nObs_Parr'
 
+# Retain the selected sites only - Parr with 4+ observations
+SELECTED_SITES <- RIVERDATA_SITE[get(Fish.var) >= eval(nObs.min), list(River, Site)]
+SELECTED_SITES <- SELECTED_SITES[order(River, decreasing=T), ]
+SELECTED_SITES$NewSite <- 1:nrow(SELECTED_SITES)
+
+# Left-join RIVERDATA
+RIVERDATA_CLEAN <- RIVERDATA[SELECTED_SITES, , on=c('River', 'Site')]
+
+# Rearrange the data frame
+RIVERDATA_CLEAN <- RIVERDATA_CLEAN[, list(River,
+                                          Site,
+                                          NewSite,
+                                          Parcelle,
+                                          Y = nParr_tot,
+                                          Velocity,
+                                          Depth,
+                                          D50)]
+
+# Save the output
+saveRDS(object   = RIVERDATA_CLEAN,
+        file     = "data/River_data_combined_final.Rds",
+        compress = "xz")
