f17466aebffb5f6c1cc885e146ecc72a98b5b6f4
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2020-05-23 12:06:52 -0400

    Very low-level statistics per sites

--- R/Data_selection.R
+++ R/Data_selection.R
@@ -14,3 +14,14 @@ library("ggplot2")        # Plotting in ggplot
 # Import clean data of SMR and PCR
 RIVERDATA <- readRDS("data/River_data_combined_with_fish_numbers.Rds")
 
+
+# Part 1 - Generate some very low level statistics -----------------------------
+
+# Calculate some basic statistics about per site data
+RIVERDATA_SITE <- RIVERDATA[,
+                            list(nFry = sum(nFry_tot),
+                                 nObs_Fry = sum(nFry_tot > 0),
+                                 nParr = sum(nParr_tot),
+                                 nObs_Parr = sum(nParr_tot > 0)),
+                            by=c('River', 'Site')]
+
