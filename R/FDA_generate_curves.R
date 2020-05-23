e7540bacc567b35ca1bd42f38a450e4b032398cb
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2020-05-23 16:59:20 -0400

    Started to generate some output curves (very messy)

--- R/FDA_generate_curves.R
+++ R/FDA_generate_curves.R
@@ -10,6 +10,7 @@
 # Import library
 library("data.table")    # Working with data.table
 library("ggplot2")       # Plotting in ggplot
+library("ggforce")       # Paginates
 
 # Import final dataset of the used sites
 RIVERDATA <- readRDS("data/River_data_combined_final.Rds")
@@ -31,7 +32,7 @@ RIVERDATA_USED$TYPE <- "Selection"
 RIVERDATA_MELT <- rbind(RIVERDATA_AVAIL, RIVERDATA_USED)
 
 # Plotting availability of each HC versus selection
-pdf("out/data visualisation/Available_versus_selected_habitat.pdf", width=8, height=4)
+pdf("out/data visualisation/Availability_selection_overall.pdf", width=8, height=4)
 print(
     ggplot(RIVERDATA_MELT, aes(x = value)) +
         geom_histogram(aes(fill=TYPE, y = ..density..), color="grey90", lwd=0.5, position = "dodge", bins=8) +
@@ -44,7 +45,7 @@ print(
 )
 dev.off()
 
-# Part 2 : Get the range of each habitat characteristics -----------------------
+# Part 3 : Get the range of each habitat characteristics -----------------------
 
 # Function - findRange :: to find the range of a vector c(xmin, xmax)
 findRange <- function(x, liminf="yes", factor=1) {
@@ -66,18 +67,22 @@ RANGE_TBL[2, 2] <- 100
 RANGE_TBL[2, 3] <- 2
 RANGE_TBL[2, 4] <- 300
 
-# Save the RANGE.TBL as a temporary output
-saveRDS(RANGE.TBL, "otmpRANGE_TBL.rds", compress="xz")
+# See the final result
+RANGE_TBL
 
-# Part 3 : Generate curves of availability and selection  ----------------------
+# Save the RANGE_TBL as a temporary output
+saveRDS(RANGE_TBL, "out/tmp/RANGE_TBL.rds", compress="xz")
+
+# Part 4 : Generate curves of availability and selection  ----------------------
 
 # Generate availability curves at each site
 SITES <- unique(RIVERDATA$NewSite)
-NSITES <- length(SITES) 
+NSITES <- length(SITES)
+
 # Function to fit a curve, scale it to 0 - 1 and adjust kernel estimate
-curve01 <- function(data, range, npoints = 2^7, adjust=1) {
+curve01 <- function(data, range, npoints = 2^8, adjust=2) {
     a <- density(x = unlist(data),
-                 bw = "SJ",
+                 bw = 'nrd0',
                  adjust = 1,
                  kernel = "gaussian",
                  n = npoints,
@@ -88,11 +93,52 @@ curve01 <- function(data, range, npoints = 2^7, adjust=1) {
 }
 
 # Generate curve01 of availability for all sites
-CURVES_DEPTH <- curve01(RIVERDATA_AVAIL[Site == 1 & variable == "Depth", value], RANGE_TBL[, "Depth"])
-CURVES_DEPTH <- cbind(CURVES_DEPTH,
-                      sapply(2:NSITES, function(w) curve01(RIVERDATA_AVAIL[NewSite == eval(w) & variable == "Depth", value], RANGE_TBL[, "Depth"])$y))
-CURVES_DEPTH <- cbind(CURVES_DEPTH,
-                      sapply(1:NSITES, function(w) curve01(RIVERDATA_USED[NewSite == eval(w) & variable == "Depth", value], RANGE_TBL[, "Depth"])$y))
-c
-olnames(CURVES_DEPTH) <- c("s", paste0("x_", SITES)), paste0("y_", SITES)
+CalcCurves <- function(w) {
 
+    # Create an empty list
+    CURVES <- list()
+
+    # Generate an initial data.table to get the s values of the x(s) curve
+    CURVES$sx<- curve01(RIVERDATA_AVAIL[NewSite == 1 & variable == w, value], RANGE_TBL[, get(w)])$
+    CURVES$y <- curve01(RIVERDATA_AVAIL[NewSite == 1 & variable == w, value], RANGE_TBL[, get(w)])
+    # Run on the NSITES availability curves x(s)
+    CURVES$x <- scbind(CURVES$x, apply(12NSITES, function(z) curve01(RIVERDATA_AVAIL[NewSite == z & variable == w, value], RANGE_TBL[, get(w)])$y)
+)
+    # Run on the NSITES selection curves y(s)
+    CURVES$y <- scbind(CURVES$y, apply(12NSITES, function(z) curve01(RIVERDATA_USED[NewSite == z & variable == w, value], RANGE_TBL[, get(w)])$y)
+)
+    # Return
+    Ccolnames(CURVES$x) <- c("s", paste0("p", 1:NSITES))
+    colnames(CURVES$y) <- c("s", paste0("p", 1:NSITES))
+
+    CURVES}
+
+# Generate curves
+CURVES_LIST <- lapply(Vars, CalcCurves)
+names(CURVES_LIST) <- Vars
+
+## Melting the element of the list for graphing purpose#melt(CURVES_LIST[[1]]$x, id.vars="s")
+
+ Plot the resulting curves
+# Plotting availability versus selection at each site
+pdf("out/data visualisation/Availability_selection_per_site.pdf")
+for (var in Vars) {
+    for (page.i in 1:2) {
+        print(
+            ggplot(CURVES_LIST[[1]]$s, aes(x = value)) +
+                geom_density(aes(fill=TYPE), color="grey90", lwd=0.1, alpha=0.3, adjust =2) +
+                #   geom_histogram(aes(fill=TYPE, y = ..density..), color="grey90", lwd=0.5, position = "dodge", bins=6) +
+                facet_wrap_paginate(~NewSite, nrow=5, ncol=4, scales="free_y", page=page.i) +
+                labs(fill="") +
+                theme(legend.position="bottom")  +
+                ylab("Probability density function (PDF)") +
+                xlab("Value") +
+                ggtitle(paste0("Habitat availability / selection for ", var)) +
+                scale_fill_manual(values = c("#9B9B93", "#63B0CD"))
+        )
+    }
+}
+dev.off()
+
+
+CURVES <- CalcCurves("Depth")
