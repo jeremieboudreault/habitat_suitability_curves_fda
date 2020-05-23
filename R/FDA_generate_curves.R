41ea8e4183eb247c8f41582444878d52bf7b054b
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2020-05-23 13:54:21 -0400

    Cleaning of the code

--- R/FDA_generate_curves.R
+++ R/FDA_generate_curves.R
@@ -14,23 +14,6 @@ library("ggplot2")       # Plotting in ggplot
 # Import final dataset of the used sites
 RIVERDATA <- readRDS("data/River_data_combined_final.Rds")
 
-# Variable - nKnots :: the number of knots for the density estimate
-nKnots <- 2^9
-
-# Function - findRange :: to find the range of a vector c(xmin, xmax)
-findRange <- function(x, liminf="yes", factor=1.1) {
-    if (liminf == "yes")
-        liminf = min(x, na.rm=T)/factor
-    return(c(liminf, max(x, na.rm=TRUE) * factor))
-}
-
-# Function - std01 :: Standardise curve to 0-1
-curve01 <- function(data, adjust=2) {
-    a <- density(unlist(data), adjust = adjust)
-    a$y <- a$y/max(a$y)
-    data.table(x=a$x, y=a$y)
-}
-
 # Variable - Vars :: the variable we are dealing with
 Vars <- c('Velocity', 'Depth', 'D50')
 
