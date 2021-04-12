2f2752e6fafcbdac1a3406a39918b85877a94b44
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2021-04-12 19:44:42 -0400

    Removed FDA_fit_models as it was empty.

--- R/FDA_fit_models.R
+++
@@ -1,8 +0,0 @@
-#  --------------------------------------------------------------------------- #
-#    Predicting habitat suitability curves through FDA 🏞                      #
-#    Created by :     Jeremie Boudreault (Jeremie.Boudreault@ete.inrs.ca)      #
-#    Current file :   FDA_fit_models.R                                         #
-#    Aim :            Fit functional regression models to HSC                  #
-#  --------------------------------------------------------------------------- #
-
-# Part 0 : Libraries, functions and variable definition ------------------------
