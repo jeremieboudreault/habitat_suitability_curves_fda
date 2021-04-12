a82a722981c84cba4676eb4802a71342aa3c36fb
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2021-04-11 20:53:03 -0400

    Renamed data_selection to s3_select_study_case.R.

--- R/s3_select_study_case.R
+++ R/s3_select_study_case.R
@@ -1,23 +1,37 @@
-#  --------------------------------------------------------------------------- #
-#    Predicting habitat suitability curves through FDA 🏞                      #
-#    Created by :     Jeremie Boudreault (Jeremie.Boudreault@ete.inrs.ca)      #
-#    Current file :   Data_selection.R                                         #
-#    Aim :            Select the variable and sites we will be working on      #
-#  --------------------------------------------------------------------------- #
+# s3_select_study_case.R
 
-# Part 0 : Libraries, functions and variable definition ------------------------
 
-# Import library
-library("data.table")    # Working with data.table
-library("ggplot2")        # Plotting in ggplot
+# Select optimal study case to be modelled (0+, 1+, 2+).
 
-# Import clean data of SMR and PCR
-RIVERDATA <- readRDS("data/River_data_combined_with_fish_numbers.Rds")
+
+# Project : habitat_suitability_curves_fsa
+# Author  : Jeremie Boudreault
+# Email   : JeremieBoudreault11@gmail.com
+# Depends : R (v3.6.3)
+# License : CC BY-NC-ND 4.0
+
+
+
+# Library ----------------------------------------------------------------------
+
+
+library(data.table)
+library(ggplot2)
+
+
+# Imports ----------------------------------------------------------------------
+
+
+# Cleaned data of SMR and PCR.
+data <- qs::qread(
+    file.path("out", "final", "smr_pcr_cleaned_final.qs")
+)
+
 
 # Part 1 - Generate some very low level statistics -----------------------------
 
 # Calculate some basic statistics about per site data
-RIVERDATA_SITE <- RIVERDATA[,
+data_SITE <- data[,
                             list(nFry = sum(nFry_tot),
                                  nObs_Fry = sum(nFry_tot > 0),
                                  nParr = sum(nParr_tot),
@@ -28,7 +42,7 @@ RIVERDATA_SITE <- RIVERDATA[,
 nObs.min <- 4
 
 # Summary of the usable sites
-RIVERDATA_SITE[, list(nSites_Fry = sum(nObs_Fry >= eval(nObs.min)),
+data_SITE[, list(nSites_Fry = sum(nObs_Fry >= eval(nObs.min)),
                       nSites_Parr = sum(nObs_Parr >= eval(nObs.min)))]
 
 # Seeing the above results, the maximim will be to have at least 4 observations
@@ -41,15 +55,15 @@ nObs.min <- 4
 Fish.var <- 'nObs_Parr'
 
 # Retain the selected sites only - Parr with 4+ observations
-SELECTED_SITES <- RIVERDATA_SITE[get(Fish.var) >= eval(nObs.min), list(River, Site)]
+SELECTED_SITES <- data_SITE[get(Fish.var) >= eval(nObs.min), list(River, Site)]
 SELECTED_SITES <- SELECTED_SITES[order(River, decreasing=T), ]
 SELECTED_SITES$NewSite <- 1:nrow(SELECTED_SITES)
 
-# Left-join RIVERDATA
-RIVERDATA_CLEAN <- RIVERDATA[SELECTED_SITES, , on=c('River', 'Site')]
+# Left-join data
+data_CLEAN <- data[SELECTED_SITES, , on=c('River', 'Site')]
 
 # Rearrange the data frame
-RIVERDATA_CLEAN <- RIVERDATA_CLEAN[, list(River,
+data_CLEAN <- data_CLEAN[, list(River,
                                           Site,
                                           NewSite,
                                           Parcelle,
@@ -59,6 +73,6 @@ RIVERDATA_CLEAN <- RIVERDATA_CLEAN[, list(River,
                                           D50)]
 
 # Save the output
-saveRDS(object   = RIVERDATA_CLEAN,
+saveRDS(object   = data_CLEAN,
         file     = "data/River_data_combined_final.Rds",
         compress = "xz")
