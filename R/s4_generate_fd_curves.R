89873672587094c8441a547d38fb590c173a7331
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2021-04-12 18:20:50 -0400

    Renamed FDA_generate_curves.R to s4_generate_fd_curves.R.

--- R/s4_generate_fd_curves.R
+++ R/s4_generate_fd_curves.R
@@ -1,40 +1,52 @@
-#  --------------------------------------------------------------------------- #
-#    Predicting habitat suitability curves through FDA 🏞                      #
-#    Created by :     Jeremie Boudreault (Jeremie.Boudreault@ete.inrs.ca)      #
-#    Current file :   FDA_generate_curves.R                                    #
-#    Aim :            Generate functional habitat suitability curves           #
-#  --------------------------------------------------------------------------- #
+# s4_generate_fd_curves.R
 
-# Part 0 : Libraries, functions and variable definition ------------------------
 
-# Import library
-library("data.table")    # Working with data.table
-library("ggplot2")       # Plotting in ggplot
-library("ggforce")       # Paginates
+# Generate functional curves for the further analysis.
 
-# Import final dataset of the used sites
-RIVERDATA <- readRDS("data/River_data_combined_final.Rds")
 
-# Variable - Vars :: the variable we are dealing with
-Vars <- c('Velocity', 'Depth', 'D50')
+# Project : habitat_suitability_curves_fsa
+# Author  : Jeremie Boudreault
+# Email   : JeremieBoudreault11@gmail.com
+# Depends : R (v3.6.3)
+# License : CC BY-NC-ND 4.0
+
+
+
+# Library ----------------------------------------------------------------------
+
+
+library(data.table)
+library(ggplot2)
+library(ggforce)
+
+
+# Imports ----------------------------------------------------------------------
+
+
+# Subsetted data for the analysis.
+data <- qs::qread(
+    x    = data_clean,
+    file = file.path("out", "final", "smr_pcr_subset_to_model.qs")
+)
+
 
 # Part 1 : Generate a data.table of availability and selection  ----------------
 
 # Create a data.table of available and used characteristics
-RIVERDATA_AVAIL <- melt(RIVERDATA, measure.vars = Vars)
-RIVERDATA_USED <- melt(RIVERDATA[Y > 0, ], measure.vars = Vars)
+data_AVAIL <- melt(data, measure.var_list = var_list)
+data_USED <- melt(data[Y > 0, ], measure.var_list = var_list)
 
 # Add an indicator
-RIVERDATA_AVAIL$TYPE <- "Availability"
-RIVERDATA_USED$TYPE <- "Selection"
+data_AVAIL$TYPE <- "Availability"
+data_USED$TYPE <- "Selection"
 
 # Merge back the two data.table
-RIVERDATA_MELT <- rbind(RIVERDATA_AVAIL, RIVERDATA_USED)
+data_MELT <- rbind(data_AVAIL, data_USED)
 
 # Plotting availability of each HC versus selection
 pdf("out/data visualisation/Availability_selection_overall.pdf", width=8, height=4)
 print(
-    ggplot(RIVERDATA_MELT, aes(x = value)) +
+    ggplot(data_MELT, aes(x = value)) +
         geom_histogram(aes(fill=TYPE, y = ..density..), color="grey90", lwd=0.5, position = "dodge", bins=8) +
         facet_wrap(~variable, nrow=2, ncol=3, scales="free") +
         labs(fill="") +
@@ -57,9 +69,9 @@ findRange <- function(x, liminf="yes", factor=1) {
 # Get the range of the three habitat characteristics
 RANGE_TBL <- data.table(
     Limit     = c("lower", "upper"),
-    Depth     = findRange(RIVERDATA$Depth, liminf = 0),
-    Velocity  = findRange(RIVERDATA$Velocity, liminf = 0),
-    D50       = findRange(RIVERDATA$D50, liminf = 0)
+    Depth     = findRange(data$Depth, liminf = 0),
+    Velocity  = findRange(data$Velocity, liminf = 0),
+    D50       = findRange(data$D50, liminf = 0)
 )
 
 # Manual adjustment for this specific dataset
@@ -76,7 +88,7 @@ saveRDS(RANGE_TBL, "out/tmp/RANGE_TBL.rds", compress="xz")
 # Part 4 : Generate curves of availability and selection  ----------------------
 
 # Generate availability curves at each site
-SITES <- unique(RIVERDATA$NewSite)
+SITES <- unique(data$NewSite)
 NSITES <- length(SITES)
 
 # Function to fit a curve, scale it to 0 - 1 and adjust kernel estimate
@@ -96,14 +108,14 @@ curve01 <- function(data, range, npoints = 2^8, adjust=4) {
 CalcCurves <- function(w) {
 
     # Generate an initial data.table to get the s values of the x(s) curve
-    x <- curve01(RIVERDATA_AVAIL[NewSite == 1 & variable == w, value], RANGE_TBL[, get(w)])
-    y <- curve01(RIVERDATA_AVAIL[NewSite == 1 & variable == w, value], RANGE_TBL[, get(w)])
+    x <- curve01(data_AVAIL[NewSite == 1 & variable == w, value], RANGE_TBL[, get(w)])
+    y <- curve01(data_AVAIL[NewSite == 1 & variable == w, value], RANGE_TBL[, get(w)])
 
     # Run on the NSITES availability curves x(s)
-    x <- data.table(cbind(x, sapply(2:NSITES, function(z) curve01(RIVERDATA_AVAIL[NewSite == z & variable == w, value], RANGE_TBL[, get(w)])$y)))
+    x <- data.table(cbind(x, sapply(2:NSITES, function(z) curve01(data_AVAIL[NewSite == z & variable == w, value], RANGE_TBL[, get(w)])$y)))
 
     # Run on the NSITES selection curves y(s)
-    y <- data.table(cbind(y, sapply(2:NSITES, function(z) curve01(RIVERDATA_USED[NewSite == z & variable == w, value], RANGE_TBL[, get(w)])$y)))
+    y <- data.table(cbind(y, sapply(2:NSITES, function(z) curve01(data_USED[NewSite == z & variable == w, value], RANGE_TBL[, get(w)])$y)))
 
     # Convert to data.table
     x$TYPE <- "Availability"
@@ -117,16 +129,16 @@ CalcCurves <- function(w) {
 
 
 # Generate curves
-CURVES_LIST <- lapply(Vars, CalcCurves)
-names(CURVES_LIST) <- Vars
+CURVES_LIST <- lapply(var_list, CalcCurves)
+names(CURVES_LIST) <- var_list
 
 # Melting the element of the list for graphing purpose
-CURVES_MELT <- lapply(CURVES_LIST, melt, id.vars=c("s", "TYPE"))
+CURVES_MELT <- lapply(CURVES_LIST, melt, id.var_list=c("s", "TYPE"))
 
 # Plot the resulting curves
 # Plotting availability versus selection at each site
 pdf("out/data visualisation/Availability_selection_curves_per_site.pdf")
-for (var in Vars) {
+for (var in var_list) {
 for (page.i in 1:2) {
         print(
             ggplot(CURVES_MELT[[var]], aes(x = s)) +
