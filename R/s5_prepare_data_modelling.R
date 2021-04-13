c8246d37c6ddb42c3a1aa8e1e8ca192344136898
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2021-04-13 19:15:22 -0400

    Added a script s5_ prior to modelling to prepare the function observations into the proper format (i.e. matrices) of Y(s) and X(r).

---
+++ R/s5_prepare_data_modelling.R
@@ -0,0 +1,81 @@
+# s5_prepare_data_modelling.R
+
+
+# Prepare data for modelling.
+
+
+# Project : habitat_suitability_curves_fda
+# Author  : Jeremie Boudreault
+# Email   : Jeremie.Boudreault [at] ete.inrs [dot] ca
+# Depends : R (v3.6.3)
+# License : CC BY-NC-ND 4.0
+
+
+
+# Library ----------------------------------------------------------------------
+
+
+library(data.table)
+
+
+# Functions --------------------------------------------------------------------
+
+
+source(file.path("R", "functions", "internals.R"))
+
+
+# Imports ----------------------------------------------------------------------
+
+
+# Functional observations.
+fd_curves <- qs::qread(
+    file.path("out", "final", "fd_curves.qs")
+)
+
+
+# Extract functional observations ----------------------------------------------
+
+
+# Y(s).
+fd_curves_y <- data.table::dcast.data.table(
+    data      = fd_curves[TYPE == "Selected", ],
+    formula   = X + VARIABLE ~ SITENEW,
+    value.var = "Y"
+)
+
+# X(r).
+fd_curves_x <- data.table::dcast.data.table(
+    data      = fd_curves[TYPE == "Available", ],
+    formula   = X + VARIABLE ~ SITENEW,
+    value.var = "Y"
+)
+
+
+# Generate dataset for all three habitat variable ------------------------------
+
+
+# Create a fd_curves_list object.
+fd_curves_list <- lapply(
+    X   = names(var_names),
+    FUN = function(var) {
+        return(list(
+            Y = t(as.matrix(fd_curves_y[VARIABLE == var, -c("X", "VARIABLE")])),
+            X = t(as.matrix(fd_curves_x[VARIABLE == var, -c("X", "VARIABLE")])),
+            s = unlist(fd_curves_y[VARIABLE == var, "X"], use.names = FALSE),
+            r = unlist(fd_curves_x[VARIABLE == var, "X"], use.names = FALSE)
+        ))
+    }
+)
+
+# Add names.
+names(fd_curves_list) <- names(var_names)
+
+
+# Exports ----------------------------------------------------------------------
+
+
+qs::qsave(
+    x = fd_curves_list,
+    file = file.path("out", "final", "fd_curves_list.qs")
+)
+
