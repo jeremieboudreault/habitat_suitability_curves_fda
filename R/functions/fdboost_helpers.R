ae1fe9897d04797448ec631039e69bf7df61102f
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2021-04-16 11:53:39 -0400

    Initial commit on the main function FDboost_kfold, which fit a function-on-function regression model with one predictor (Y(s) ~ B(s, r)X(r)) while performing a k-fold cross validation. For now, only the leave-one-out cv is implemented (n_folds == n_obs).

--- R/functions/fdboost_helpers.R
+++ R/functions/fdboost_helpers.R
@@ -76,3 +76,80 @@ calc_fun_metric <- function(y_hat, y_obs, metric = "frmse") {
 
 }
 
+
+# Fit a FDboost model with k-fold cross-validation -----------------------------
+
+
+# Function-on-function regression model to fit Y(s) ~ B(s, r) X(r) while
+# performing a k-fold cross-validation to find optimal mstop based on a given
+# performance criterion.
+
+FDboost_kfold <- function(
+    data,
+    n_folds,
+    fdboost_opt
+) {
+
+    # Checks on dimension of supplied data.
+    if (nrow(data$Y) =! nrow(data$X)   |
+        ncol(data$Y) =! length(data$s) |
+        ncol(data$X) =! length(data$r)) {
+        stop("Dimension of supplied data do not match.", call. = FALSE)
+    }
+
+    # Number of observations.
+    n_obs <- nrow(data$Y)
+
+    # Number of points for Y.
+    n_s <- length(data$s)
+
+    # Check on the number of folds.
+    if (!is.integer(n_folds) | n_folds < 1L | n_folds > n_obs) {
+        stop("Invalid number of folds.", call. FALSE)
+    }
+
+    # Generate folds.
+    folds <- generate_fold(n_obs, n_folds)
+
+    # Set cluster for parralel computation.
+    cl <- parallel::makeCluster(parallel::detectCores() - 1L)
+
+    # Apply in parallel the .FDboost_kfold_k function.
+    res <- parallel::parLapplyLB(
+        X            = seq.int(1L, n_folds),
+        fun          = .FDboost_kfold_k,
+        data         = data,
+        folds        = folds,
+        fdboost_opts = fdboost_opts,
+        cl           = cl
+    )
+
+    # Close cluster.
+    parallel::stopCluster(cl)
+
+    # Create an array to rearrange results in a nicer format.
+    results <- array(NA, dim = c(n_obs, n_s, fdboost_opts$mstop_max))
+
+    # Rearrange results to the current position in the array.
+    for (i in seq_len(length(res))) {
+        results[i, , ] <- t(res[[i]])
+    }
+
+    # Calculate the chosen metric.
+    error <- apply(
+        X      = results,
+        MARGIN = 3L,
+        FUN    = calc_fun_metric,
+        y_obs  = data$Y,
+        err    = fdboost_opts$metric
+    )
+
+    # Find optimal mstop based on the value of the metric.
+    mstop_best <- which.min(error)
+
+    # Return results.
+    return(structure(results,
+        mstop_best = mstop_best
+    ))
+
+}
