a816d3482a29dff270ad6a512cc591272abdb969
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2021-04-17 14:51:59 -0400

    Added a new script s8_ to calculate models performance on both the k_fold cross validation and the full model.

---
+++ R/s8_calculate_frm_performance.R
@@ -0,0 +1,80 @@
+# s8_calculate_frm_performance.R
+
+
+# Calculate functional regression models performance.
+
+
+# Project : habitat_suitability_curves_fda
+# Author  : Jeremie Boudreault
+# Email   : Jeremie.Boudreault [at] ete.inrs [dot] ca
+# Depends : R (v3.6.3)
+# License : CC BY-NC-ND 4.0
+
+
+
+# Library ----------------------------------------------------------------------
+
+
+library(data.table)
+library(ggplot2)
+library(FDboost)
+
+
+# Functions --------------------------------------------------------------------
+
+
+source(file.path("R", "functions", "internals.R"))
+source(file.path("R", "functions", "fdboost_helpers.R"))
+
+
+# Imports ----------------------------------------------------------------------
+
+
+# Fitted functional models.
+frm_results <- qs::qread(
+    file = file.path("out", "tmp", "s6_frm_results.qs")
+)
+
+
+# Calculate metrics for all models ---------------------------------------------
+
+
+# Apply for each models.
+performance <- lapply(
+    X   = names(var_names),
+    FUN = function(w) {
+
+        # Extract observations and results.
+        res <- frm_results[[w]]
+        curves <- fd_curves[[w]]
+
+        # Full model.
+        full_perf <- calc_fun_metrics(
+            y_hat  = res$full$y_hat,
+            y_obs  = res$full$y_obs
+        )
+
+        # K-fold metrics.
+        kfold_perf <- calc_fun_metrics(
+            y_hat  = res$y_hat_kfold[, , which(res$mstops == res$mstop_best)],
+            y_obs  = res$full$y_obs
+        )
+
+        # Return results.
+        return(rbind(full_perf, kfold_perf))
+
+    }
+)
+
+# Add names to performance.
+names(performance) <- names(var_names)
+
+
+# Export results ---------------------------------------------------------------
+
+
+qs::qsave(
+    x    = performance,
+    file = file.path("out", "tmp", "s8_frm_performance.qs")
+)
+
