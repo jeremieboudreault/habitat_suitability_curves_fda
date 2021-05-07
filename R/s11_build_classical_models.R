8772a01d4df78e634db46b07de0a5984503d5f29
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2021-05-07 15:49:51 -0400

    Added a new script s11_ to build classical models for comparison purpose.

---
+++ R/s11_build_classical_models.R
@@ -0,0 +1,16 @@
+# s11_build_classical_models.R
+
+
+# Step 11 : Build classical models at local and regional level for comparison.
+
+
+# Project : habitat_suitability_curves_fda
+# Author  : Jeremie Boudreault
+# Email   : Jeremie.Boudreault [at] ete.inrs [dot] ca
+# Depends : R (v3.6.3)
+# License : CC BY-NC-ND 4.0
+
+
+
+# Library ----------------------------------------------------------------------
+
