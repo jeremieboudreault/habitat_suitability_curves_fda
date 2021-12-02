a142c0a6d8ccd28ee9a553ba6d4844722a13873a
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2021-12-02 16:34:52 -0500

    Initial commit on s13_compute_HSI, a further step to be done.

---
+++ R/s13_compute_hsi.R
@@ -0,0 +1,19 @@
+# s13_compute_hsi.R
+
+
+# Step 13 : Compute Habitat Suitability Index (HSI) on each parcel.
+
+
+# Project : habitat_suitability_curves_fda
+# Author  : Jeremie Boudreault
+# Email   : Jeremie.Boudreault [at] ete.inrs [dot] ca
+# Depends : R (v3.6.3)
+# License : CC BY-NC-ND 4.0
+
+
+
+# Library ----------------------------------------------------------------------
+
+
+library(data.table)
+library(ggplot2)
